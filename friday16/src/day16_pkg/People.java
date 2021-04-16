a7c131a419a72dabdd399e12e94a2b703ea32407
user <user@DESKTOP-QQG1134>
2021-04-16 14:38:48 +0900

    getter/setter 이용 toString 메소드 선언하면 객체이름만 출력 가능

---
+++ friday16/src/day16_pkg/People.java
@@ -0,0 +1,79 @@
+package day16_pkg;
+
+public class People {
+
+	private String name;
+	private String rNumber;
+	private String address;
+	private int age;
+	
+	People(){
+		
+	}
+	
+	//마우스 우클릭>source>Generate constructor using fields
+
+	public People(String name, String rNumber, String address, int age) {
+		super();//부모클래스가 가지고 있는 생성자를 호출
+		this.name = name;
+		this.rNumber = rNumber;
+		this.address = address;
+		this.age = age;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getrNumber() {
+		return rNumber;
+	}
+
+
+
+	public void setrNumber(String rNumber) {
+		this.rNumber = rNumber;
+	}
+
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	//마우스 우클릭>source>Generate toString()...
+	@Override
+	public String toString() {
+		return "People [name=" + name + ", rNumber=" + rNumber + ", address=" + address + ", age=" + age + "]";
+	}
+	
+	
+	
+}
---
+++ friday16/src/day16_pkg/PeopleMain.java
@@ -0,0 +1,49 @@
+package day16_pkg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PeopleMain {
+
+	public static void main(String[] args) {
+		
+		People p=new People();
+		p.setName("호랑이");
+		p.setrNumber("11111-11111");
+		p.setAddress("숲");
+		p.setAge(100);
+		
+		People p1=new People();
+		p1.setName("여우");
+		p1.setrNumber("22222-22222");
+		p1.setAddress("골짜기");
+		p1.setAge(90);
+		
+		People p2=new People();
+		p2.setName("사슴");
+		p2.setrNumber("33333-333333");
+		p2.setAddress("들판");
+		p2.setAge(80);
+		
+		List<People>peoList=new ArrayList<People>();
+		peoList.add(p);
+		peoList.add(p1);
+		peoList.add(p2);
+		
+		System.out.println(peoList.get(0).getName());
+		
+		for(int i=0; i<peoList.size(); i++) {
+			System.out.println(peoList.get(i).getName());
+			System.out.println(peoList.get(i).getrNumber());
+			System.out.println(peoList.get(i).getAddress());
+			System.out.println(peoList.get(i).getAge());
+		}
+		for(int i=0; i<peoList.size(); i++) {
+			System.out.println(peoList.get(i));
+		}
+		System.out.println(peoList.get(0));
+		System.out.println(p1.toString());
+		
+	}
+
+}
